82dd962f68aada99ac3183375e7a883bd24a752d
RLjumber <ronaldjumber@gmail.com>
2023-03-13 14:18:14 -0400

    added some basic typography and button components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from "@mui/material";
+import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container, Button } from "@mui/material";
 import { PhotoCamera } from "@mui/icons-material";
+import { makeStyles } from "@material"
 
 
 const App = () => {
@@ -18,12 +19,26 @@ const App = () => {
       </AppBar>
       <main>
         <div>
-          <Container maxWidth="sm">
-            <Typography variant="h2" align="center" color="textPrimary" gutterBottom>Photo Album</Typography>
+          <Container maxWidth="sm" style={{ marginTop: '50px' }}>
+            <Typography variant="h2" align="center" color="textPrimary" gutterBottom>Poopy Pants I pooped in them...</Typography>
             <Typography variant="h5" align="center" color="textSecondary" paragraph>
-              Whats going on guyss!!! How does this look like? I hope this is long enough because this needs to be a paragraph element!
+              Whats going on guyss!!! Ali pooped her pants and then wiped it all over the ground while yelling "WHATS OBAMAS LAST NAME HUH KYLE" and then she bit a young child and blamed it on OBAMA too? At least thats what I heard.
             </Typography>
           </Container>
+          <div>
+            <Grid container spacing={2} justifyContent="center">
+              <Grid item>
+                  <Button variant="contained" color="primary">
+                    See the Smear
+                  </Button>
+              </Grid>
+              <Grid item>
+                  <Button variant="contained" color="primary">
+                    Secondary Angle
+                  </Button>
+              </Grid>
+            </Grid>
+          </div>
         </div>
       </main>
     </>
